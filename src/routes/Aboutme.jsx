69717ec89ff6c85887bcc76054a4dad0c39159ce
Melvin González <54681730+melagora@users.noreply.github.com>
2024-11-05 17:46:24 -0600

    last change Aboutme 100%

--- src/routes/Aboutme.jsx
+++ src/routes/Aboutme.jsx
@@ -3,9 +3,7 @@ import developer from "../assets/programming-amico.svg";
 
 import "../styles/styles.css";
 
-
 export default function Aboutme() {
-
   return (
     <section className="general_section">
       <div className="content_aboutme">
@@ -17,22 +15,26 @@ export default function Aboutme() {
             tecnologías modernas en proyectos reales.
           </p>
           <p>
-            He trabajado en varios proyectos donde he implementado React Routes,
-            React Hooks y React Forms, desarrollando componentes reutilizables y
-            estilizados con CSS puro para mantener interfaces consistentes y
-            atractivas.
+            He trabajado en varios proyectos donde he implementado 
+            <b>
+              <i> React Routes, React Hooks y React Forms</i>
+            </b>
+            , desarrollando componentes reutilizables y estilizados con CSS puro
+            para mantener <mark>interfaces consistentes y atractivas.</mark>
           </p>
           <p>
             Estoy en búsqueda de oportunidades para colaborar en equipos
             dinámicos donde pueda aportar y seguir aprendiendo de la mano de
-            profesionales experimentados en el desarrollo web. ¡No dudes en
-            contactarme si deseas establecer una conexión o discutir
-            oportunidades potenciales!
+            profesionales experimentados en el desarrollo web.
+          </p>
+          <p>
+            ¡No dudes en contactarme si deseas establecer una conexión o
+            discutir oportunidades potenciales!
           </p>
         </div>
         <div className="content2_aboutme">
           <div className="content2_aboutme_img">
-            <img src={developer} alt="Perfil" className="animate-down"  />
+            <img src={developer} alt="Perfil" className="animate-down" />
           </div>
         </div>
       </div>
