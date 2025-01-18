f73300bf2b0b70dd4a57ccfca96eb72506dbfd43
Melvin González <54681730+melagora@users.noreply.github.com>
2025-01-18 01:04:44 -0600

    Changes of auto ajust

--- src/routes/Contact.jsx
+++ src/routes/Contact.jsx
@@ -2,11 +2,17 @@ import React from "react";
 import "@/styles/styles.css";
 
 export default function Contact() {
+  const pad = { paddingLeft: "10px" };
+
   return (
     <div className="general_section">
       <div className="contentContact">
         <h1>- Contacto -</h1>
         <section>
+          <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.5.0/font/bootstrap-icons.min.css"
+          />
           <p className="subtittleContact">Correo electronico personal:</p>
           <div>
             <button
@@ -14,14 +20,21 @@ export default function Contact() {
               onClick={() => window.open("mailto:mgonzalezramos21@gmail.com")}
             >
               Enviar correo
+              <i style={pad} className="bi bi-envelope-fill"></i>
             </button>
           </div>
         </section>
         <section>
           <p className="subtittleContact"> Redes Sociales:</p>
           <div className="redesContacto">
-            <button className="buttonContact">Instagram</button>
-            <button className="buttonContact">Facebook</button>
+            <button className="buttonContact">
+              Instagram
+              <i style={pad} className="bi bi-instagram"></i>
+            </button>
+            <button className="buttonContact">
+              Facebook
+              <i style={pad} className="bi bi-facebook"></i>
+            </button>
           </div>
         </section>
         <section>
@@ -32,8 +45,9 @@ export default function Contact() {
               target="_blank"
               rel="noopener noreferrer"
             >
-              Click para visitar el perfil de IN
+              Click para visitar el perfil de
             </a>
+            <i style={pad} className="bi bi-linkedin"></i>
           </button>
         </section>
         <section>
@@ -44,8 +58,9 @@ export default function Contact() {
               target="_blank"
               rel="noopener noreferrer"
             >
-              Click para visitar el perfil de GIT
+              Click para visitar el perfil de
             </a>
+            <i style={pad} className="bi bi-github"></i>
           </button>
         </section>
       </div>
