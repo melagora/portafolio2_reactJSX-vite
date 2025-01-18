cc98f54ba5e5ec2271f533b7bf71c823b8e76dc5
Melvin González <54681730+melagora@users.noreply.github.com>
2025-01-18 01:32:13 -0600

    las changes links contact

--- src/routes/Contact.jsx
+++ src/routes/Contact.jsx
@@ -13,7 +13,11 @@ export default function Contact() {
             rel="stylesheet"
             href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.5.0/font/bootstrap-icons.min.css"
           />
-          <p className="subtittleContact">Correo electronico personal:</p>
+          <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
+          />
+          <p className="subtittleContact">Correo electrónico personal:</p>
           <div>
             <button
               className="buttonContact"
@@ -45,7 +49,7 @@ export default function Contact() {
               target="_blank"
               rel="noopener noreferrer"
             >
-              Click para visitar el perfil de
+              Clic para visitar el perfil de
             </a>
             <i style={pad} className="bi bi-linkedin"></i>
           </button>
@@ -58,9 +62,36 @@ export default function Contact() {
               target="_blank"
               rel="noopener noreferrer"
             >
-              Click para visitar el perfil de
+              Clic para visitar el perfil de
             </a>
             <i style={pad} className="bi bi-github"></i>
+          </button>
+        </section>
+        <h1 style={{ paddingTop: "20px" }}>- Más Info -</h1>
+        <section>
+          <p className="subtittleContact">Alura Latam + Oracle:</p>
+          <button className="buttonContact">
+            <a
+              href="https://app.aluracursos.com/user/melagora/fullCertificate/d28d32bc68f5d351c5759e03ab4a72aa"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+               Ver certificados de finalización:
+            </a>
+            <i style={pad} class="fa-solid fa-user-graduate"></i>
+          </button>
+        </section>
+        <section>
+          <p className="subtittleContact">Microsoft Learns:</p>
+          <button className="buttonContact">
+            <a
+              href="https://learn.microsoft.com/es-es/users/melagora/transcript/dz6g4skwlg33825"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Ver módulos completados de:
+            </a>
+            <i style={pad} class="fa-brands fa-windows"></i>
           </button>
         </section>
       </div>
